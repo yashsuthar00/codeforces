913d68ed10918c754d5aabcbd05340a278ba1581
Yash Suthar <132425549+yashsuthar00@users.noreply.github.com>
2025-08-28 13:02:52 +0530

    Add Codeforces solution for problem 318A

---
+++ java/318A.java
@@ -0,0 +1,19 @@
+// 318 A. Even Odds
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scannr(System.in);
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+
+        long odds = (n + 1) / 2;
+        
+        if(k<= odds) {
+            System.out.println(2 * k - 1);
+        } else {
+            System.out.println(2 * (k - odds));
+        }
+    }
+}
